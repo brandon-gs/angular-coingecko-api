8d5a6b56cecf0bc6d1e5170d31530c537496d5af
Brandon GS <brandongs180@gmail.com>
2022-07-20 02:08:15 -0500

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  image: string;
+  name: string;
+  symbol: string;
+  current_price: string;
+  price_change_percentage_24h: number;
+  total_volume: number;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  api =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+  coins: Coin[] = [];
+  filteredCoins: Coin[] = [];
+  titles: string[] = ['#', 'Coin', 'Price', 'Price change', '24h volume'];
+  searchText = '';
+
+  constructor(private http: HttpClient) {}
+
+  searchCoin(): void {
+    this.filteredCoins = this.coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+        coin.symbol.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+
+  ngOnInit(): void {
+    this.http.get<Coin[]>(this.api).subscribe((res) => {
+      this.coins = res;
+      this.filteredCoins = res;
+    });
+  }
+}
